39e0e9457da37505b062f5a502d68736524b1f81
Ben Wheeler <wheeler.benjamin@gmail.com>
2014-11-24 12:13:38 -0500

    db fixes, fonts, assets, js

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Technosocial
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+
+
+    # load custom modules
+    config.autoload_paths += Dir["#{config.root}/lib/modules/"]
+
   end
 end
