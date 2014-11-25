39903f53619fd7a5255a075dcd387450b171c14b
Ben Wheeler <wheeler.benjamin@gmail.com>
2014-11-25 17:31:35 -0500

    first draft of graph, some responsive design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,4 +71,95 @@ function getStyleRuleValue(style, selector, sheetparam) {
 
 
 
+$(function () {
+        Highcharts.getOptions().plotOptions.bar.colors = (function () {
+        var colors = [],
+            base = Highcharts.getOptions().colors[0],
+            i;
+
+        for (i = 0; i < 10; i += 1) {
+            // Start out with a darkened base color (negative brighten), and end
+            // up with a much brighter color
+            colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
+        }
+        return colors;
+    }());
+
+
+    $('#dayo_graph').highcharts({
+        chart: {
+            type: 'bar',
+            spacing: [0, 0, 0, 0],
+            backgroundColor: 'rgba(0, 0, 0, 0)'
+
+        },
+                        credits: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+                title: {
+            text: null,
+                },
+        legend: {
+            enabled: false
+        },
+        yAxis: {
+            tickPosition: "inside",
+            tickInterval: .1,
+            tickLength: 2,
+            max: 1,
+            title: {
+            text: null
+        },
+            labels: {
+                enabled: false
+            }
+        },
+        xAxis: {
+                               tickLength: 0,
+            labels: {
+                enabled: false
+            }
+        },
+        plotOptions: {
+                               bar: {
+                               pointWidth: 25,
+                                           colorByPoint: true,
+                               colors: this.colors
+
+                               },
+            series: {
+                stacking: 'normal',
+                            dataLabels: {
+                                color: 'white',
+                    enabled: true,
+                    useHTML: true,
+                               y: 0,
+
+                    formatter: function () {
+    var num = this.percentage;
+        return '<div style="top: 0">' + num + "% " + this.series.name + '</div>';
+                    }
+
+                }
+            }
+
+        },
+        series: [{
+            name: 'Javascript',
+            data: [.5]
+        }, {
+            name: 'Rails',
+            data: [.2]
+        }, {
+            name: 'Python',
+            data: [.3]
+        }]
+    });
+});
+
+
+
 
