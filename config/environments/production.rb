b842ea1777836c9bb577efe4ddf034bc53468a4e
Ben Wheeler <wheeler.benjamin@gmail.com>
2015-01-05 18:17:12 -0500

    incremented compiled asset version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0.1'
+  config.assets.version = '1.0.2'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['*.js', '*.css', '*.css.erb', '*.scss.erb']
+  #config.assets.precompile += ['*.js', '*.css', '*.css.erb', '*.scss.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
