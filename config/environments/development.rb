0057faa6285893d6cb09425ef88b722c0f4fc062
Ben Wheeler <wheeler.benjamin@gmail.com>
2015-03-24 11:00:46 -0400

    precompile dev into dev-assets not assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # added this so that local precomiling in production vs development mode store in different places
-#  config.assets.prefix = "/dev-assets"
+  config.assets.prefix = "/dev-assets"
 end
